b8add2b1b3030741be13bd23ca7a232e903a98d6
alex_ <alexis.comven@gmail.com>
2023-03-01 16:28:09 -0700

    InicioMaster(alx)

---
+++ lib/roman.dart
@@ -0,0 +1,14 @@
+int  numero = 0;
+int romano = 0;
+String res ='';
+
+String conversion  (int num1, int numr){
+
+  int num = num1;
+  String rom = numr;
+
+  if(num == 1){
+    rom = 'I';
+  }
+  return '';
+}
---
+++ test/roman_test.dart
@@ -0,0 +1,8 @@
+import 'package:roman/roman.dart';
+import 'package:test/test.dart';
+
+void main() {
+  
+
+  
+}
